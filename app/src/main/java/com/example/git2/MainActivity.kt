e658155fc8a0a405ba57fe757089dd9e6ef21e53
chesselbach <87391036+chesselbach@users.noreply.github.com>
2021-11-11 09:42:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/git2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.git2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// This assignment is the assignment to do a simple pizza ordering app.
+// The app uses buttons, text fields and an image.
+// You have a youtube video but also a 'shortcut' paper to explain how
+// Watch the video and then follow the steps in the paper ( or just
+// follow the video
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
